f6bab74b1d3c0d161cd23adec02bd6de8efd912c
Prab <pgogana@opentable.com>
2023-11-22 13:30:34 +1100

    feat: Add New Project

---
+++ TestPRMergeApp/TestPRMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPRMergeApp.swift
+//  TestPRMergeApp
+//
+//  Created by Prabhjot Singh Gogana on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestPRMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
