80cfb9cbde84aeb67f252af918b64797a7cbc67d
Prab <pgogana@opentable.com>
2024-04-16 12:10:31 +1000

    chore: add on hello world 6

--- TestPRMergeApp/ContentView.swift
+++ TestPRMergeApp/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             Text("Hello, world 3!")
             Text("Hello, world 4!")
             Text("Hello, world 5!")
+            Text("Hello, world 6!")
         }
         .padding()
     }
