c0507a055d4a3f4050b95c8292a6472b0a9bb74d
Prab <116533915+PrabhJay@users.noreply.github.com>
2025-10-14 09:46:22 +1100

    Fix indentation in ContentView.swift
    Signed-off-by: Prab <116533915+PrabhJay@users.noreply.github.com>

--- TestPRMergeApp/ContentView.swift
+++ TestPRMergeApp/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+                          var body: some View {
         VStack {
-            Image(systemName: "globe")
+                Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world 1!")
+                    Text("Hello, world 1!")
             Text("Hello, world 2!")
-            Text("Hello, world 3!")
+                            Text("Hello, world 3!")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
